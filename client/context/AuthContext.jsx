13db8a46b0d5ae43dc5e4480600d05e99d503128
Kapilansh Patil <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-08-19 00:28:02 +0530

    feat: Add jwt-decode library and implement token validation in AuthContext

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export  const AuthContext = createContext();
 
@@ -6,13 +6,35 @@ export const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null)
 
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if(token) {
+            try {
+                const decodedToken = jwtDecode(token);
+
+                if (decodedToken.exp * 1000 > Date.now()){
+                    setUser(decodedToken.user)
+                }
+                else {
+                    localStorage.removeItem('token');
+                }
+            } catch (error) {
+                console.error('Failed to decode token',error);
+                localStorage.removeItem('token')
+            }
+        }
+    })
+
     const login = (userData) => {
         setUser(userData)
     }
 
     const logout = () => {
         setUser(null)
+        localStorage.removeItem('token')
     }
+
 
     return (
         <AuthContext.Provider value={ {login, user, logout} }>
